4a160befe26e14d37eb04668d85cc9f48b8bc059
anamasantos <anamasantos94@gmail.com>
2024-11-09 21:05:03 -0300

    modulo25_aula147 email no endpoint.go

--- internal/endpoints/campaigns_post_test.go
+++ internal/endpoints/campaigns_post_test.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	"bytes"
+	"context"
 	"emailn/internal/contract"
 	"encoding/json"
 	"fmt"
@@ -15,17 +16,31 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+func setup(body contract.NewCampaign, createdByExpected string) (*http.Request, *httptest.ResponseRecorder) {
+	var buf bytes.Buffer
+	json.NewEncoder(&buf).Encode(body)
+	req, _ := http.NewRequest("POST", "/", &buf)
+	ctx := context.WithValue(req.Context(), "email", createdByExpected)
+	req = req.WithContext(ctx)
+	res := httptest.NewRecorder()
+	return req, res
+}
+
 func Test_CampaignPost_should_save_new_campaign(t *testing.T) {
 	assert := assert.New(t)
+	createdByExpected := "teste1@teste.com.br"
 	body := contract.NewCampaign{
-		Name:    "teste",
-		Content: "Hi everyone",
-		Emails:  []string{"teste@test.com"},
-		Status:  "Pending",
+		Name:      "teste",
+		Content:   "Hi everyone",
+		Emails:    []string{"teste@test.com"},
+		Status:    "Pending",
+		CreatedBy: createdByExpected,
 	}
 	service := new(internalmock.CampaignServiceMock)
 	service.On("Create", mock.MatchedBy(func(request contract.NewCampaign) bool {
-		if request.Name == body.Name && request.Content == body.Content {
+		if request.Name == body.Name &&
+			request.Content == body.Content &&
+			request.CreatedBy == createdByExpected {
 			return true
 		} else {
 			return false
@@ -33,10 +48,7 @@ func Test_CampaignPost_should_save_new_campaign(t *testing.T) {
 	})).Return("34x", nil)
 	handler := Handler{CampaignService: service}
 
-	var buf bytes.Buffer
-	json.NewEncoder(&buf).Encode(body)
-	req, _ := http.NewRequest("POST", "/", &buf)
-	res := httptest.NewRecorder()
+	req, res := setup(body, createdByExpected)
 	_, status, err := handler.CampaignPost(res, req)
 	assert.Equal(201, status)
 	assert.Nil(err)
@@ -54,10 +66,7 @@ func Test_CampaignPost_should_inform_error_when_exist(t *testing.T) {
 	service.On("Create", mock.Anything).Return("", fmt.Errorf("error"), "")
 	handler := Handler{CampaignService: service}
 
-	var buf bytes.Buffer
-	json.NewEncoder(&buf).Encode(body)
-	req, _ := http.NewRequest("POST", "/", &buf)
-	res := httptest.NewRecorder()
+	req, res := setup(body, "teste@teste.com.br")
 	_, _, err := handler.CampaignPost(res, req)
 	assert.NotNil(err)
 
